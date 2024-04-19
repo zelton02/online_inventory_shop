8a48911868dace2464a91280968b0617c6cdc5f7
zelton02 <zhiheng426@gmail.com>
2024-04-19 21:43:25 +0800

    Updated web.php

--- routes/web.php
+++ routes/web.php
@@ -21,36 +21,38 @@ use App\Http\Controllers\OrderController;
 |
 */
 
-// Authentication Routes
 Auth::routes();
 
 // Public Routes
 Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/home', [HomeController::class, 'index'])-> name('home');
 
-// User Login Routes
+// User & Admin Login Route
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
-// User Registration Routes
+// User Registration Route
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'create']);
 Route::get('/registration/success', [RegisterController::class, 'registrationSuccess'])->name('registration.success');
 
 Route::middleware(['auth'])->group(function () {
-    // Product routes
+    // User Product Route
+    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 
-    // Cart routes
+    // User Cart Route
     Route::get('/cart', [CartController::class, 'index'])->name('cart.show');
     Route::post('/product/addToCart/{id}', [CartController::class, 'addToCart'])->name('cart.addToCart');
     Route::post('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
     Route::delete('/cart/delete/{id}', [CartController::class, 'remove'])->name('cart.remove');
     Route::get('/cart/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
 
-    // Payment routes
+    // User Payment Route
     Route::post('payment/process', [PaymentController::class, 'process'])->name('payment.process');
 
-    // User routes
+    // User Profile Management
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::view('/user/create', 'user.addUser')->name('user.create');
     Route::post('/user/create', [UserController::class, 'store'])->name('user.store');
     Route::get('/user/update/{id}', [UserController::class, 'showUpdate'])->name('user.showpebUpdate');
@@ -58,32 +60,28 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('/user/delete/{id}', [UserController::class, 'destroy'])->name('user.destroy');
     Route::get('/user/{id}', [UserController::class,'checkDetails'])->name('user.checkDetails');
 
-    // Order routes
+    // User Order Route
     Route::get('/orders', [OrderController::class, 'index'])->name('order.show');
-
-    // Logout route
-    Route::get('logout', [LoginController::class,'logout']);
 });
 
-// Admin protected routes
+// Admin Protected Route
 Route::group(['middleware' => 'admin'], function () {
     Route::get('/admin', function () {
         return view('adminDashboard');
     });
 
-    // Products routes
-    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    // Admin Product Operation Route
     Route::view('/product/create', 'product.addProduct')->name('product.create');
     Route::post('/product/create', [ProductController::class, 'store'])->name('products.store');
     Route::get('/product/update/{id}', [ProductController::class, 'showUpdate'])->name('product.showUpdate');
     Route::post('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
     Route::delete('/product/delete/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
 
-    // User routes
-    Route::get('/user', [UserController::class, 'index'])->name('user.index');
 
-    // Orders routes
+    // Admin Order Operation Route
     Route::get('/admin/orders', [OrderController::class, 'index'])->name('admin.orders.index');
     Route::get('/admin/orders/{id}/showUpdate', [OrderController::class, 'showUpdate'])->name('admin.order.showUpdate');
     Route::post('/admin/orders/{id}/update', [OrderController::class, 'update'])->name('admin.order.update');
 });
+
+Route::get('logout', [LoginController::class,'logout']);
